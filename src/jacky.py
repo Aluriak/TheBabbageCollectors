e80c57d2eb0b624df7f43d6f0a51d3b59e13990f
Aluriak <lucas.bourneuf@laposte.net>
2014-01-25 23:37:08 +0100

    use Dijkstra+correct directionOf()+correct creatGraph()

--- src/jacky.py
+++ src/jacky.py
@@ -13,6 +13,7 @@ SCORE_DISTANCE = 1
 
 
 
+
 class JackyBot(Bot):
     """Our Bot !"""
 
@@ -39,24 +40,23 @@ class JackyBot(Bot):
         for key in self.game.mines_locs.iterkeys():
             # if mine is not Jacky's mine
             if self.game.mines_locs[key] != self.hero.id:
-                path = self.graph.shortestPath(self.hero.pos, key)
+                path = self.graph.Dijkstra(self.hero.pos, key)
                 if path == None:
                     # No path founded !
                     print 'move: No path founded'
                     return 'Stay'
-                print path
                 if len(finalPath) == 0 or len(path) < len(finalPath) :
                     finalPath = path
         # Get direction of 2nd node in finalPath (2nd node = adjacent neighbor)
-        direction = self.directionOf(finalPath[0]) 
+        direction = self.directionOf(finalPath[1]) 
         # RETURN
         return direction
 
     def directionOf(self, neighbor):
         """Return direction of move for go in targeted neighbors"""
         y, x = neighbor
-        x = x - self.hero.pos[1]
-        y = y - self.hero.pos[0]
+        x = x - self.hero.pos[0]
+        y = y - self.hero.pos[1]
         direction = game.AIMreversed[(y,x)]
         print direction
         return direction
@@ -70,19 +70,19 @@ class JackyBot(Bot):
         for row in range(len(self.game.board.tiles)):
             for col in range(len(self.game.board.tiles[row])):
 
-                neighbors = []
-                #print "C: "+str((row, col)) # DEBUG
-                # for each neighbors
-                for coord in [(row-1, col), (row+1, col), (row, col+1), (row, col-1)]:
-                    # add it only if passable 
-                    if self.game.board.exist(coord) and self.game.board.notAWall(coord):
-                        #print "\t"+str(coord) # DEBUG
-                        neighbors.append(coord)
+                # if tile exist and is praticable
+                if self.game.board.exist((row,col)) and self.game.board.notAWall((row,col)):
 
-                # add neighbors as successors
-                self.graph.addSuccTo((row, col), neighbors)
-        print self.graph
+                    neighbors = []
+                    # for each neighbors
+                    for coord in [(row-1, col), (row+1, col), (row, col+1), (row, col-1)]:
+                        # add it only if passable and valable in board
+                        if self.game.board.exist(coord) and  self.game.board.notAWall(coord):
+                            neighbors.append(coord)
 
+                    # add neighbors as successors
+                    self.graph.addSuccTo((row, col), neighbors)
+        #print self.graph
 
 
 
