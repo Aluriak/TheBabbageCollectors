34568c716fae44615f0527543d202d4a25f862a2
Aluriak <lucas.bourneuf@laposte.net>
2014-01-25 13:40:44 +0100

    New functions

--- vindinium-starter-python/jacky.py
+++ vindinium-starter-python/jacky.py
@@ -1,4 +1,5 @@
 from random import choice
+from game import Game
 from bot import Bot 
 
 
@@ -7,17 +8,23 @@ class JackyBot(Bot):
     """Our Bot !"""
 
     def __init__(self):
-        """"""
+        """Jacky is creat !"""
 
     def move(self, state):
-        """"""
+        """Call at each step of game. Must return a direction choose between ['Stay', 'North', 'South', 'East', 'West'] """
+        # INIT
         directions = ['Stay', 'North', 'South', 'East', 'West']
+        # CHOOSE A DIRECTION
         direction = choice(directions)
+        # RETURN
         return direction
 
 
     def creatGraph(self, state):
-        pass
+        """Creation of graph of map. Use of Game class in module game
+        Initialization of self.graph."""
+        self.graph = {} # graph of map
+
 
 
 
