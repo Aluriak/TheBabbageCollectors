dac2e024dcab9fd3779a3677817882c4641fbe73
Aluriak <lucas.bourneuf@laposte.net>
2014-01-26 08:33:41 +0100

    test code

--- samples/graphs/main.py
+++ samples/graphs/main.py
@@ -1,32 +1,32 @@
 from graph import Graph
 
-#nodes = {
-    #'A' : ['B', 'E'],
-    #'B' : ['A', 'F', 'C', 'D'],
-    #'C' : ['B', 'H'],
-    #'D' : ['B', 'G'],
-    #'E' : ['A', 'F'],
-    #'F' : ['E', 'B', 'G'],
-    #'G' : ['F', 'D', 'H'],
-    #'H' : ['C', 'G'],
-#}
+nodes = {
+    'A' : ['B', 'E'],
+    'B' : ['A', 'F', 'C', 'D'],
+    'C' : ['B', 'H'],
+    'D' : ['B', 'G'],
+    'E' : ['A', 'F'],
+    'F' : ['E', 'B', 'G'],
+    'G' : ['F', 'D', 'H'],
+    'H' : ['C', 'G'],
+}
 
 
-#g = Graph(nodes)
+g = Graph(nodes)
 
-#print g 
+print g 
 
 
 
-#for startNode in g.nodes.keys():
-    #for endNode in g.nodes.keys():
-        #shortestPath = g.Dijkstra(startNode,endNode)
-        #print startNode+"to"+endNode+": ",
-        #if len(shortestPath) == 0: 
-            #print "No way !"
-        #else:
-            #print ", ".join( shortestPath )
-    #print "END"
+for startNode in g.nodes.keys():
+    for endNode in g.nodes.keys():
+        shortestPath = g.Dijkstra(startNode,endNode)
+        print startNode+"to"+endNode+": ",
+        if len(shortestPath) == 0: 
+            print "No way !"
+        else:
+            print ", ".join( shortestPath )
+    print "END"
 
 notFoundYet = [0,3,5,8,3,6,10,-1]
 
